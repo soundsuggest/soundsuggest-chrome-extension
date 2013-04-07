19bc5ccaa117e737b4defa73464fe06ca1dc886f
JorisSchelfaut <joris.schelfaut@hotmail.com>
2013-04-07 13:06:23 +0200

    soundsuggest chrome extension background js addrecommendation etc

--- js/background.js
+++ js/background.js
@@ -1,80 +1,58 @@
-/*
+/* *****************************************************************************
+ * background.js
+ * 
+ * Processes calls to the chrome extension background page. Used for local
+ * storage and calls to other websites and APIs.
+ * 
  * Part of the SoundSuggest project. For more info consult:
  * http://soundsuggest.wordpress.com/ . Written by
  * Joris Schelfaut.
- */
-
-/*
- * NOTE : to make this file work, you'll need to change the
- * URL used in "js/lib/lastfm/lastfm.api.js" from
- * http://ws.audioscrobbler.com/2.0/ to https://ws.audioscrobbler.com/2.0/ ,
- * as Chrome Extensions only allow HTTPS calls, as specified in
- * "manifest.json" under  "content_security_policy" : "script-src 'self'
- * https://ws.audioscrobbler.com/2.0/; object-src 'self'".
- * 
- */
+ **************************************************************************** */
 
 // LISTENERS :
 // -----------
-/* 
- * Listen for any changes to the URL of any tab.
- */
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
-/*
- * The onMessage method is a part of chrome.extension
- * http://developer.chrome.com/extensions/extension.html
- */
-//chrome.extension.onMessage.addListener(loadRecommendedArtists);
-
-chrome.extension.onMessage.addListener(storageAction);
-
+chrome.extension.onMessage.addListener(doAction);
 
 // GLOBAL VARIABLES AND CONSTANTS :
 // --------------------------------
-var api_key     = '828c109e6a54fffedad5177b194f7107';
-var api_secret  = '7c2f09e6eb84e8a6183c59e0bc574f70';
-var cache       = new LastFMCache();
-var lastfm      = new LastFM({
-    apiKey    : api_key,
-    apiSecret : api_secret,
-    cache     : cache
+var API_KEY      = '828c109e6a54fffedad5177b194f7107';
+var API_SECRET   = '7c2f09e6eb84e8a6183c59e0bc574f70';
+var LAST_FM      = new LastFM({
+    apiKey      : API_KEY,
+    apiSecret   : API_SECRET,
+    cache       : new LastFMCache()
 });
+var DATA_SERVICE = '';
+var SESSION_KEY  = '';
+var USERNAME     = '';
 
 // FUNCTIONS :
 // -----------
-/**
- * Called when the url of a tab changes.
- * @param {type} tabId
- * @param {type} changeInfo
- * @param {type} tab
- * @returns {undefined}
- */
-function checkForValidUrl(tabId, changeInfo, tab) {
+checkForValidUrl = function(tabId, changeInfo, tab) {
     if (tab.url.indexOf('last.fm/home') > -1) {
         chrome.pageAction.show(tabId);
     }
 };
 
-// https://developer.chrome.com/extensions/storage.html
-function storeValue(value, key) {
-    //chrome.storage.local.set(dataObj, function() { /*...*/ });
-    // instead of anonumous object { key : value }
-    
-}
+doAction = function(request, sender, sendResponse) {
+    var action = request.action.toString().toLowerCase().split(".");
+    if ("storage" === action[0]) {
+        storageAction(action[1], request, sendResponse);
+    } else if ("lastfm" === action[0]) {
+        lastfmAction(action[1], request, sendResponse);
+    }
+};
 
-function retrieveValue(key) {
-    
-}
-
-function storageAction(request, sender, sendResponse) {
-    if (request.action === 'storage.set') {
+storageAction = function(action, request, sendResponse) {
+    if (action === 'set') {
         console.log('Storage action storage.set');
         var dataObj = {}; dataObj[request.data.key] = request.data.value;
         chrome.storage.local.set(dataObj, function() {
             console.log('Value [' + request.data.value + '] was saved with key [' + request.data.key + '].');
             sendResponse({ success : true });
         });
-    } else if (request.action === 'storage.get') {
+    } else if (action === 'get') {
         console.log('Storage action storage.get');
         chrome.storage.local.get(request.data.key, function(value) {
             console.log('Value [' + value[request.data.key] + '] was retrieved with key [' + request.data.key + '].');
@@ -84,79 +62,37 @@ function storageAction(request, sender, sendResponse) {
         console.error('Undefined storage action.');
     }
     return true;
-}
-
-/**
- * 
- * @param {type} request
- * @param {type} sender
- * @param {type} sendResponse
- * @returns {Boolean}
- */
-function loadRecommendedArtists(request, sender, sendResponse) {
-    if (request.action === 'user.getrecommendedartists') {
-        //if (! localStorage["lastfm_session"]) {
-        if (! retrieveValue("lastfm_session")) {
-            console.log("Get a session object");
-            lastfm.auth.getSession({
-                token: request.data.token
-            }, {
-                success: function(data_sess) {
-                    console.log("Retrieved a session object from Last.fm API.");
-                    //localStorage.setItem ('lastfm_session', data_sess.session);
-                    //console.log('A session key was saved : ' + localStorage['lastfm_session'].key);
-                    
-                    storeValue(data_sess.session, 'lastfm_session');
-                    console.log('A session key was saved : ' + retrieveValue("lastfm_session").key);
-                    
-                    getRecommendedArtists(request.data.user, request.data.limit);
-                },
-                error: function(data_sess_error) {
-                    console.log(data_sess_error.error + " : " + data_sess_error.message);
-                }
-            });
-        } else {
-            getRecommendedArtists(request.data.user, request.data.limit);
-        }
-        return true;
-    }
-    return false;
 };
 
-/**
- * 
- * @param {type} user
- * @param {type} limit
- * @returns {undefined}
- */
-function getRecommendedArtists(user, limit) {
-    lastfm.user.getRecommendedArtists({
-        user    : user,
-        limit   : limit
-    },
-    {
-        //key : localStorage['lastfm_session'].key
-        key : retrieveValue('lastfm_session').key
-        // session object : http://www.last.fm/api/show/auth.getSession
-        /*
-         * {
-         *      key
-         *      name
-         *      subscriber
-         * }
-         */
-        // only key is really relevant - key lifetime is infinite --> store for each user
-    },
-    {
-        success: function(data_recs) {
-            for (var i = 0; i < data_recs.recommendations.artist.length; i++) {
-                console.log("data.recommendations.artist[" + i + "] = "
-                        + data_recs.recommendations.artist[i].name);
-            }
-            sendResponse({ data : data_recs });
+lastfmAction = function(action, request, sendResponse) {
+    if (action === 'datastructure') {
+        d3.json(DATA_SERVICE + "?key=" + SESSION_KEY + "&user=" + USERNAME, function(error, data) {
+            sendResponse(data);
+        });
+    } else if (action === 'addrecommendation') {
+        var artistname = request.params.artist;
+        LAST_FM.library.addArtist({
+            artist  : artistname,
+            api_key : API_KEY
         },
-        error: function(data_recs_error) {
-            console.log(data_recs_error.error + " : " + data_recs_error.message);
-        }
-    });
-}
+        {
+            key : SESSION_KEY
+        },
+        {
+            success: function(data) {
+                console.log(data);
+                sendResponse({
+                    session     : SESSION_KEY,
+                    username    : USERNAME,
+                    added       : artistname
+                });
+            },
+            error: function(data_error) {
+                console.error(data_error.error + " : " + data_error.message);
+            }
+        });
+    } else {
+        console.error('Undefined last.fm action.');
+    }
+    return true;
+};
--- js/main.js
+++ js/main.js
@@ -1,64 +1,12 @@
-/*
- * Part of the SoundSuggest project. For more info consult:
- * http://soundsuggest.wordpress.com/ . Written by
- * Joris Schelfaut.
- */
-// First : check if a session key was stored
-
-// IF NOT : fetch new token from last.fm API
-
-/*
-console.log("Start script.");
-var token = $.url().param('token');
-console.log("Token received : " + token);
-
-if (!token) {
-    console.log("Redirect - retrieve and authenticate token.");
-    window.location = 'http://www.last.fm/api/auth/?api_key=828c109e6a54fffedad5177b194f7107&cb=http://last.fm/home/';
-} else {
-    console.log("Token received : " + token);
-    chrome_getRecommendedArtists({
-        user    : 'noisedriver',
-        limit   : 20,
-        token   : token
-    },
-    function(data_recs) {
-        var list = '<ol>';
-        for (var i = 0; i < data_recs.data.recommendations.artist.length; i++) {
-            list += '<li>' + data_recs.data.recommendations.artist[i].name + '</li>';
+function addRecommendation(artist) {
+    chrome.extension.sendMessage({
+        action      : 'lastfm.addrecommendation',
+        session     : SESSION_KEY,
+        params      : {
+            artist : artist
         }
-        jQuery(list + '</ol>').appendTo('#homeHead');
+    },
+    function(data) {
+        // Refresh.
     });
 }
-console.log("End script.");
-*/
-
-console.log("+-Start script.");
-
-var KEY = 'name';
-var VALUE = 'Joris';
-
-console.log("| chrome.extension.sendMessage");
-console.log("|\taction : storage.set");
-chrome.extension.sendMessage({
-        action  : 'storage.set',
-        data    : {
-            key : KEY,
-            value : VALUE
-        }
-    },
-    function (a) {
-        console.log('|\t-->Successfully stored value with corresponding key!');
-        console.log("| chrome.extension.sendMessage");
-        console.log("|\taction : storage.get");
-        chrome.extension.sendMessage({
-                action  : 'storage.get',
-                data    : {
-                    key : KEY
-                }
-            },
-            function (a) {
-                console.log('|\t-->Retrieved : ' + a.value);
-                console.log("+-End script.");
-            });
-    });
