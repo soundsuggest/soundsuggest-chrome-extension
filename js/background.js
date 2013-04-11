e010552d90abc2f20b1d0dff600c5d0967f16fe6
JorisSchelfaut <joris.schelfaut@hotmail.com>
2013-04-11 11:08:21 +0200

    chrome extension implmentation - still needs some updates

--- js/background.js
+++ js/background.js
@@ -21,42 +21,49 @@ var API_SECRET   = '7c2f09e6eb84e8a6183c59e0bc574f70';
 var LAST_FM      = new LastFM({
     apiKey      : API_KEY,
     apiSecret   : API_SECRET,
-    cache       : new LastFMCache()
+    cache       : new LastFMCache(),
+    apiUrl      : 'https://ws.audioscrobbler.com/2.0/'
 });
-var DATA_SERVICE = '';
+var DATA_SERVICE = 'https://1.x8-esoteric-code-c.appspot.com/';
 var SESSION_KEY  = '';
 var USERNAME     = '';
+var DEBUG        = true;
+
+if (DEBUG) console.log("Global variables loaded.");
 
 // FUNCTIONS :
 // -----------
-checkForValidUrl = function(tabId, changeInfo, tab) {
+function checkForValidUrl(tabId, changeInfo, tab) {
     if (tab.url.indexOf('last.fm/home') > -1) {
         chrome.pageAction.show(tabId);
     }
 };
 
-doAction = function(request, sender, sendResponse) {
+function doAction(request, sender, sendResponse) {
+    if (DEBUG) console.log("background.js#doAction");
     var action = request.action.toString().toLowerCase().split(".");
     if ("storage" === action[0]) {
         storageAction(action[1], request, sendResponse);
     } else if ("lastfm" === action[0]) {
-        lastfmAction(action[1], request, sendResponse);
+        lastfmAction(action[1] + "." + action[2], request, sendResponse);
     }
+    return true;
 };
 
-storageAction = function(action, request, sendResponse) {
+function storageAction(action, request, sendResponse) {
+    if (DEBUG) console.log("background.js#storageAction");
     if (action === 'set') {
-        console.log('Storage action storage.set');
-        var dataObj = {}; dataObj[request.data.key] = request.data.value;
+        if (DEBUG) console.log('Storage action storage.set');
+        var dataObj = {}; dataObj[request.params.key] = request.data.value;
         chrome.storage.local.set(dataObj, function() {
-            console.log('Value [' + request.data.value + '] was saved with key [' + request.data.key + '].');
+            if (DEBUG) console.log('Value [' + request.params.value + '] was saved with key [' + request.params.key + '].');
             sendResponse({ success : true });
         });
     } else if (action === 'get') {
-        console.log('Storage action storage.get');
-        chrome.storage.local.get(request.data.key, function(value) {
-            console.log('Value [' + value[request.data.key] + '] was retrieved with key [' + request.data.key + '].');
-            sendResponse({ value : value[request.data.key] });
+        if (DEBUG) console.log('Storage action storage.get');
+        chrome.storage.local.get(request.params.key, function(value) {
+            if (DEBUG) console.log('Value [' + value[request.params.key] + '] was retrieved with key [' + request.params.key + '].');
+            sendResponse({ value : value[request.params.key] });
         });
     } else {
         console.error('Undefined storage action.');
@@ -64,12 +71,17 @@ storageAction = function(action, request, sendResponse) {
     return true;
 };
 
-lastfmAction = function(action, request, sendResponse) {
-    if (action === 'datastructure') {
-        d3.json(DATA_SERVICE + "?key=" + SESSION_KEY + "&user=" + USERNAME, function(error, data) {
-            sendResponse(data);
+function lastfmAction(action, request, sendResponse) {
+    if (DEBUG) console.log("background.js#lastfmAction");
+    if (action === 'recommender.load') {
+        USERNAME = request.params.username;
+        chrome.storage.local.get('SESSION_KEY', function(value) {
+            d3.json(DATA_SERVICE + "?key=" + value['SESSION_KEY'] + "&user=" + USERNAME, function(error, data) {
+                sendResponse(data);
+            });
         });
-    } else if (action === 'addrecommendation') {
+        
+    } else if (action === 'recommender.add') {
         var artistname = request.params.artist;
         LAST_FM.library.addArtist({
             artist  : artistname,
@@ -91,6 +103,22 @@ lastfmAction = function(action, request, sendResponse) {
                 console.error(data_error.error + " : " + data_error.message);
             }
         });
+    } else if (action === 'auth.getsession') {
+        LAST_FM.auth.getSession({
+                token: request.params.token
+            }, {
+                success: function(data_sess) {
+                    SESSION_KEY = data_sess.session.key;
+                    var dataObj = {}; dataObj['SESSION_KEY'] = SESSION_KEY;
+                    chrome.storage.local.set(dataObj, function() {
+                        console.log('Value [' + SESSION_KEY + '] was saved with key [\'SESSION_KEY\'].');
+                        sendResponse({ key : SESSION_KEY });
+                    });
+                },
+                error : function(data_error) {
+                    console.error(data_error.error + " : " + data_error.message);
+                }
+            });
     } else {
         console.error('Undefined last.fm action.');
     }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,182 @@
-function addRecommendation(artist) {
+var SPINNER;
+var WHITEBOX;
+var SESSION_KEY;
+var USERNAME;
+var TOKEN;
+var API_KEY = '828c109e6a54fffedad5177b194f7107';
+var DEBUG   = true;
+var HIDDEN  = false;
+
+jQuery(document).ready(function() {
+    if (DEBUG) console.log("Starting Chrome Extension.");
+    getUsername();
+    createLayout();
+    if (DEBUG) console.log("Layout created.");
+    startSpinner();
+    if (DEBUG) console.log("Spinner created.");
+    if (DEBUG) console.log("Loading session data.");
+    getSession();
+});
+
+
+/**
+ * <p>
+ * Creates the <em>HTML</em> layout for that will be
+ * injected into the Last.fm page by the Chrome Extension.
+ * </p>
+ * @returns {undefined}
+ */
+createLayout = function() {
+    if (DEBUG) console.log("main.js#createLayout");
+    jQuery('<div class="soundsuggest" id="soundsuggest"></div>')
+        .insertAfter('h1.heading');
+    jQuery('#soundsuggest')
+        .append('<div class="soundsuggest-chart" id="chart"></div>');
+    jQuery('#soundsuggest')
+        .append('<div class="soundsuggest-users" id="users"></div>');
+    jQuery('#soundsuggest')
+        .append('<div class="soundsuggest-item-info" id="item-info"></div>');
+    jQuery('#chart')
+        .append('<div id="spinner"></div>');
+};
+
+/**
+ * <p>
+ * Loads the JSON data and instantiates a Whitebox object that
+ * creates the visualization.
+ * </p>
+ * @returns {undefined}
+ */
+loadVisualization = function() {
+    if (DEBUG) console.log("main.js#loadVisualization");
     chrome.extension.sendMessage({
-        action      : 'lastfm.addrecommendation',
+        action  : 'lastfm.recommender.load',
+        params  : {
+            username : USERNAME
+        }
+    },
+    function (data) {
+        SPINNER.stop();
+        WHITEBOX = new Whitebox();
+        WHITEBOX.create(data);
+    });
+};
+
+/**
+ * <p>
+ * Initializes the <code>SESSION_KEY</code> global parameter. The method
+ * works as follows :
+ * </p>
+ * <pre>
+ * IF key IN local_storage THEN
+ *      loadVisualization
+ * ELSE
+ *      IF token IN url
+ *          get_session
+ *          loadVisualization
+ *      ELSE
+ *          get_token
+ *          get_session
+ *          loadVisualization
+ *      END-IF
+ *  END-IF
+ * </pre>
+ * @param {type} action
+ * @returns {undefined}
+ */
+getSession = function(action) {
+    if (DEBUG) console.log("main.js#getSession");
+    chrome.extension.sendMessage({
+        action  : 'storage.get',
+        params  : {
+            key : 'SESSION_KEY'
+        }
+    },
+    function (a) {
+        SESSION_KEY = a.value;
+        if (DEBUG) console.log('SESSION_KEY == ' + SESSION_KEY);
+        if (! SESSION_KEY) {
+            TOKEN = $.url().param('token');
+            if (! TOKEN) window.location = 'http://www.last.fm/api/auth/?api_key=' + API_KEY + '&cb=' + window.location;
+            lastfm.api.chrome.auth.getSession({
+                token   : TOKEN,
+                api_key : API_KEY
+            }, function(data) {
+                SESSION_KEY = data.key;
+                if (DEBUG) console.log('SESSION_KEY == ' + SESSION_KEY);
+                loadVisualization();
+            });
+        } else {
+            loadVisualization();
+        }
+    });
+};
+
+/**
+ * <p>
+ * Initializes the global parameter <code>USERNAME</code>.
+ * </p>
+ * @returns {undefined}
+ */
+getUsername = function() {
+    if (DEBUG) console.log("main.js#getUsername");
+    USERNAME = $('#idBadgerUser').attr('href').split("user/")[1];
+    if (DEBUG) console.log('USERNAME == ' + USERNAME);
+};
+
+/**
+ * <p>
+ * Initializes the global parameter <code>SPINNER</code> and starts
+ * the Spinner.
+ * </p>
+ * @returns {undefined}
+ */
+startSpinner = function() {
+    SPINNER = new Spinner({
+        lines: 13,
+        length: 20,
+        width: 10,
+        radius: 50,
+        corners: 1,
+        rotate: 0,
+        direction: 1,
+        color: '#000',
+        speed: 1,
+        trail: 60,
+        shadow: false,
+        hwaccel: false,
+        className: 'spinner',
+        zIndex: 2e9,
+        top: '200px',
+        left: 'auto'
+    }).spin(document.getElementById("spinner"));
+};
+
+/**
+ * <p>
+ * Adds a given artist recommendation to the active user's library
+ * and refreshes the visualization.
+ * </p>
+ * @param {type} artist
+ * @returns {undefined}
+ */
+addRecommendation = function(artist) {
+    if (DEBUG) console.log("main.js#addRecommendation");
+    chrome.extension.sendMessage({
+        action      : 'lastfm.recommender.add',
         session     : SESSION_KEY,
         params      : {
             artist : artist
         }
     },
     function(data) {
-        // Refresh.
+        if (DEBUG) console.log(data);
+        WHITEBOX.destroy();
+        loadVisualization();
     });
-}
+};
+
+toggle_hide = function() {
+    if (DEBUG) console.log("main.js#toggle_hide");
+    // hides the visualization.
+};
